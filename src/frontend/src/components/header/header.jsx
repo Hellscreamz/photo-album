d16a56969e899d05b5a7a290a7af5f308bda4e9a
MARTIN IVANOV <martoxyox@gmail.com>
2023-10-07 00:59:58 +0300

    adjust the last 10 photos

--- src/frontend/src/components/header/header.jsx
+++ src/frontend/src/components/header/header.jsx
@@ -1,16 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
+import LastTenPhotos from '../last-uploaded-pics/last-uploaded-pics';
 
 function HeaderMenu() {
-    return (
-        <header>
-          <nav>
-            <a href="/">GetStarted</a>
-            <a href="/pictures">Pictures</a>
-            <a href="/users">Users</a>
-            <a href="/contacts">Contacts</a>
-          </nav>
-        </header>
-      );
+  const [showLastTenPhotos, setShowLastTenPhotos] = useState(false);
+
+  const handleGetStartedClick = (e) => {
+    e.preventDefault();
+    setShowLastTenPhotos(true);
+  };
+
+  return (
+    <header>
+      <nav>
+        <a href="/" onClick={handleGetStartedClick}>GetStarted</a>
+        <a href="/pictures">Pictures</a>
+        <a href="/users">Users</a>
+        <a href="/contacts">Contacts</a>
+      </nav>
+      {showLastTenPhotos && <LastTenPhotos />}
+    </header>
+  );
 }
 
 export default HeaderMenu;
